01d6b733ed2a7d7046badc13f4ba14837a4143ee
Bielomega04 <bielmega@gmail.com>
2025-04-21 10:53:08 +0200

    21/04/2025
    Finished code of palantir

--- Palantir/src/main.cpp
+++ Palantir/src/main.cpp
@@ -6,18 +6,19 @@
 #define BLUE 9  // actualy red
 #define IR_PIN 3
 
-#define KEY_1  0XF30CFF00
-#define KEY_2  0XE718FF00
-#define KEY_4  0xF708FF00
-#define KEY_5  0XE31CFF00
+#define KEY_1 0XF30CFF00
+#define KEY_2 0XE718FF00
+#define KEY_4 0xF708FF00
+#define KEY_5 0XE31CFF00
 const int DELAY = 10;
 const int DELAY2 = 5000;
 int v_red = random(0, 255), v_green = random(0, 255), v_blue = random(0, 255);
-unsigned long lastCall, waitTime = 2000;
-bool modoFijo = false;
+unsigned long waitTime = 2000;
+unsigned long time;
+unsigned long key_pressed = 0;
 
 IRrecv irrecv(IR_PIN);
-decode_results results; 
+decode_results results;
 //----------------------------------------------
 void viewRemoteValues();
 void testLED();
@@ -27,6 +28,7 @@ void colorElf(int &v_red, int &v_green, int &v_blue);
 void colorHobbit(int &v_red, int &v_green, int &v_blue);
 void colorDwarf(int &v_red, int &v_green, int &v_blue);
 void colorHuman(int &v_red, int &v_green, int &v_blue);
+void readIRRemote();
 //----------------------------------------------
 void setup()
 {
@@ -37,50 +39,18 @@ void setup()
     irrecv.enableIRIn();
     testLED();
 }
+
 void loop()
 {
-    if (irrecv.decode(&results)) 
+    time = millis();
+    if (time % waitTime == 0)
     {
-        unsigned long codigo = results.value; 
-        irrecv.resume(); 
-
-        if (codigo != 0xFFFFFFFF) 
-        {                        // Evita repeticiones del mando
-            lastCall = millis(); // Reinicia el temporizador
-            modoFijo = true;     // Fija el color seleccionado
-            Serial.print("Código recibido: ");
-            Serial.println(codigo, HEX);
-
-            switch (codigo) 
-            {
-            case KEY_1: 
-                colorElf(v_red, v_green, v_blue);
-                break; 
-            case KEY_2:
-                colorHobbit(v_red, v_green, v_blue);
-                break;
-            case KEY_4:
-                colorDwarf(v_red, v_green, v_blue);
-                break;
-            case KEY_5:
-                colorMagician(v_red, v_green, v_blue);
-                break;
-            default:
-                Serial.println("Código desconocido");
-                break;
-            }
-            Serial.println("Fin de la secuencia");
-        }
+        neutralColor(v_red, v_blue, v_green);
     }
-    if (millis() - lastCall > waitTime) 
+    else
     {
-        if(modoFijo){
-            Serial.println("Fin del modo fijo");   
-        }
-        modoFijo = false; // Vuelve al modo aleatorio
-        neutralColor(v_red, v_green, v_blue);
+        readIRRemote();
     }
-    delay(10);
 }
 
 //----------------------------------------------
@@ -94,7 +64,7 @@ void colorElf(int &v_red, int &v_green, int &v_blue)
 //----------------------------------------------
 void colorHobbit(int &v_red, int &v_green, int &v_blue)
 {
-    int v_red_f = 255, v_green_f = 255, v_blue_f = 0; // yellow
+    int v_red_f = 255, v_green_f = 100, v_blue_f = 0; // yellow
     changeColor(v_red, v_green, v_blue, v_red_f, v_green_f, v_blue_f);
     Serial.println("Hobbit");
     delay(DELAY2);
@@ -110,7 +80,7 @@ void colorDwarf(int &v_red, int &v_green, int &v_blue)
 //----------------------------------------------
 void colorHuman(int &v_red, int &v_green, int &v_blue)
 {
-    int v_red_f = 138, v_green_f = 149, v_blue_f = 151; // grey
+    int v_red_f = 25, v_green_f = 49, v_blue_f = 120; // grey
     changeColor(v_red, v_green, v_blue, v_red_f, v_green_f, v_blue_f);
     Serial.println("Human");
     delay(DELAY2);
@@ -223,3 +193,33 @@ void changeColor(int &v_red, int &v_green, int &v_blue, int v_red_f, int v_green
     v_green = v_green_f;
     v_blue = v_blue_f;
 }
+//----------------------------------------------
+void readIRRemote()
+{
+    if (IrReceiver.decode())
+    {
+        Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);
+        key_pressed = IrReceiver.decodedIRData.decodedRawData;
+        switch (key_pressed)
+        {
+        case KEY_1:
+            colorElf(v_red, v_green, v_blue);
+            break;
+        case KEY_2:
+            colorHobbit(v_red, v_green, v_blue);
+            break;
+        case KEY_4:
+            colorDwarf(v_red, v_green, v_blue);
+            break;
+        case KEY_5:
+            colorHuman(v_red, v_green, v_blue);
+            break;
+        default:
+            viewRemoteValues();
+            break;
+        }
+        IrReceiver.resume();
+        key_pressed = 0;
+    }
+}
+//----------------------------------------------
